0cfd61dd47f2c690ee3578fd2749ba011ca70c41
yangbo <1832064870@qq.com>
2019-12-12 17:31:53 +0800

    编辑

--- client/src/pages/Account/Editor/index.tsx
+++ client/src/pages/Account/Editor/index.tsx
@@ -1,17 +1,34 @@
 import Taro, { Config, useEffect, useState } from "@tarojs/taro";
 import { View } from "@tarojs/components";
-import { AtInput, AtList } from "taro-ui";
+import { AtInput, AtList, AtButton, AtFloatLayout } from "taro-ui";
 import "./index.scss";
+
+const defProps = {
+  key: '',
+  value: ''
+}
 
 export default function AccountDetail() {
   const _params: com.Account = this.$router.params;
   const [params, setParams] = useState(_params);
   const { title = '', username, password, ...other } = params;
 
+  const [visible, setVisible] = useState(false);
+  const [properties, setProperties] = useState(defProps);
+
   useEffect(() => {
     Taro.setNavigationBarTitle({ title });
   }, []);
   
+  function addPropertiesHandler() {
+    setParams({
+      ...params,
+      [properties.key]: properties.value
+    })
+    setProperties(defProps);
+    setVisible(false);
+  }
+
   console.log(params);
 
   const keys = Object.keys(other);
@@ -51,9 +68,9 @@ export default function AccountDetail() {
           }}
         />
 
-        {keys.map((item, index) => (
+        {keys.map((item) => (
           <AtInput
-            key={index}
+            key={item}
             name="other"
             title={item}
             type='text'
@@ -65,6 +82,42 @@ export default function AccountDetail() {
           />
         ))}
       </AtList>
+
+      <View className="btn_wrapper">
+        <AtButton type="primary" className="add_btn">保存修改</AtButton>
+        <AtButton className="add_btn" onClick={() => setVisible(true)}>新增字段</AtButton>
+      </View>
+      
+
+      <AtFloatLayout
+        isOpened={visible}
+        title="新增字段"
+        onClose={() => setVisible(false)}
+      >
+        <AtInput
+          name="title"
+          title="属性名"
+          type='text'
+          placeholder='请输入新增属性名'
+          value={properties.key}
+          onChange={(v: string) => {
+            setProperties({ ...properties, key: v })
+          }}
+        />
+        <AtInput
+          name="title"
+          title="属性值"
+          type='text'
+          placeholder='请输入新增属性值'
+          value={properties.value}
+          onChange={(v: string) => {
+            setProperties({ ...properties, value: v })
+          }}
+        />
+        <AtButton type="primary" className="add_btn" onClick={addPropertiesHandler}>
+          确认
+        </AtButton>
+      </AtFloatLayout>
     </View>
   );
 }
