385797be8555b772ff7d5104a1ce7d28f576ff9e
yangbo <1832064870@qq.com>
2020-03-26 20:07:22 +0800

    update: 用户表中新增用户类型

--- cloud/functions/user/index.js
+++ cloud/functions/user/index.js
@@ -30,7 +30,8 @@ exports.main = async (event, context) => {
       province: '',
       isLock: true,  // 是否启用加锁
       isFingerprintLock: false, // 是否启用指纹解锁
-      isNinecaseLock: false // 是否启用九宫格解锁
+      isNinecaseLock: false, // 是否启用九宫格解锁
+      type: 3  // 1: 超级管理员  2: 管理员  3：普通用户  4：付费用户
     }
 
     let userInfo = await user.where({
